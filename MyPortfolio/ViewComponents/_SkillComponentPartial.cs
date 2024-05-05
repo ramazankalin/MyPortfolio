0d4318d14d7edd88eaa529138960cd61f2a7125f
ramazankalin <ramazankalin73@gmail.com>
2024-05-05 17:53:13 +0300

    Add project files.

---
+++ MyPortfolio/ViewComponents/_SkillComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _SkillComponentPartial : ViewComponent
+    {
+        MyPortfolioContext myPortfolioContext = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            var values = myPortfolioContext.Skills.ToList();
+            return View(values);
+        }
+    }
+}
